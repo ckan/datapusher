f8035b898666443dba7908536e3b224357a07cda
Dominik Moritz <domoritz@gmail.com>
2012-12-21 14:45:15 +0100

    Use data converters in datastorer and refactor code

--- importerservice/datastorer.py
+++ importerservice/datastorer.py
@@ -1,69 +1,49 @@
 import json
 import requests
-import datetime
 import itertools
 import tempfile
 
-import messytables
-from messytables import (CSVTableSet, XLSTableSet, types_processor,
-                         headers_guess, headers_processor, type_guess,
-                         offset_processor)
-
 import ckanserviceprovider.job as job
 import ckanserviceprovider.util as util
+import dataconverters as dc
 
 
 TYPE_MAPPING = {
-    messytables.types.StringType: 'text',
-    messytables.types.IntegerType: 'numeric',  # 'int' may not be big enough,
+    'String': 'text',
+    'Integer': 'numeric',  # 'int' may not be big enough,
                     # and type detection may not realize it needs to be big
-    messytables.types.FloatType: 'float',
-    messytables.types.DecimalType: 'numeric',
-    messytables.types.DateType: 'timestamp',
-    messytables.types.DateUtilType: 'timestamp'
+    'Float': 'float',
+    'Decimal': 'numeric',
+    'DateTime': 'timestamp'
 }
 
 
 def check_response(response, datastore_create_request_url):
-    try:
-        if not response.status_code:
-            raise util.JobError('Datastore is not reponding at %s with '
-                    'response %s' % (datastore_create_request_url, response))
-    except Exception, e:
-        pass
-        #datastorer_upload.retry(exc=e)
+    if not response.status_code:
+        raise util.JobError('Datastore is not reponding at %s with '
+                'response %s' % (datastore_create_request_url, response))
 
     if response.status_code not in (201, 200):
         try:
-            # try logging a json response but ignore it if the content is not json
             response.content = json.loads(response.content)
-        except:
-            pass
-        raise util.JobError('Datastorer bad response code (%s) on %s. Response was %s' %
+            raise util.JobError('Datastorer bad response code (%s) on %s. Response was %s' %
                 (response.status_code, datastore_create_request_url, response.content))
+        except:
+            raise util.JobError('Datastorer bad response code (%s) on %s.' %
+                (response.status_code, datastore_create_request_url))
 
 
-def stringify_processor():
-    def to_string(row_set, row):
-        for cell in row:
-            if not cell.value:
-                cell.value = None
-            else:
-                cell.value = unicode(cell.value)
-        return row
-    return to_string
-
-
-def datetime_procesor():
-    ''' Stringifies dates so that they can be parsed by the db
-    '''
-    def datetime_convert(row_set, row):
-        for cell in row:
-            if isinstance(cell.value, datetime.datetime):
-                cell.value = cell.value.isoformat()
-                cell.type = messytables.StringType()
-        return row
-    return datetime_convert
+# generates chunks of data that can be loaded into ckan
+# n is the maximum size of a chunk
+def chunky(iterable, n):
+    it = iter(iterable)
+    while True:
+        chunk = list(
+            itertools.imap(
+                dict, itertools.islice(it, n)))
+        if not chunk:
+            return
+        yield chunk
 
 
 def check_provided_data(data):
@@ -81,61 +61,51 @@ def import_into_datastore(task_id, input):
     data = input['data']
     check_provided_data(data)
 
+    ckan_url = data['ckan_url'].rstrip('/')
+    datastore_create_request_url = '%s/api/action/datastore_create' % (ckan_url)
+
     excel_types = ['xls', 'application/ms-excel', 'application/xls',
                    'application/vnd.ms-excel']
+    excel_xml_types = ['xlsx']
     tsv_types = ['tsv', 'text/tsv', 'text/tab-separated-values']
+    csv_types = ['csv', 'text/csv', 'text/comma-separated-values']
 
-    result = requests.get(data['url'])
+    requested_resource = requests.get(data['url'])
 
-    content_type = result.headers['content-type']\
+    content_type = requested_resource.headers['content-type']\
                                     .split(';', 1)[0]  # remove parameters
 
+    def is_of_type(types):
+        return content_type in types or data['format'] in types
+
     with tempfile.NamedTemporaryFile() as f:
-        f.write(result.text)
+        f.write(requested_resource.text)
         f.flush()
         f.seek(0)
 
-        if content_type in excel_types or data['format'] in excel_types:
-            table_sets = XLSTableSet(f)
-        else:
-            is_tsv = (content_type in tsv_types or
-                      data['format'] in tsv_types)
-            delimiter = '\t' if is_tsv else ','
-            table_sets = CSVTableSet(f, delimiter=delimiter, encoding='utf-8')
+        # TODO: refactor
+        t = ''
+        if is_of_type(excel_types):
+            t = 'xls'
+        elif is_of_type(excel_xml_types):
+            t = 'xlsx'
+        elif is_of_type(csv_types):
+            t = 'csv'
+        elif is_of_type(tsv_types):
+            t = 'tsv'
 
-        ##only first sheet in xls for time being
-        row_set = table_sets.tables[0]
-        offset, headers = headers_guess(row_set.sample)
-        row_set.register_processor(headers_processor(headers))
-        row_set.register_processor(offset_processor(offset + 1))
-        row_set.register_processor(datetime_procesor())
+        # TODO: why do I need target?
+        data_for_conversion = dc.dataconverter(
+            f, {'type': t, 'target': 'json', 'header_type': '1'})
+        fields, result = data_for_conversion.convert()
 
-        #logger.info('Header offset: {0}.'.format(offset))
-
-        guessed_types = type_guess(
-            row_set.sample,
-            [
-                messytables.types.StringType,
-                messytables.types.IntegerType,
-                messytables.types.FloatType,
-                messytables.types.DecimalType,
-                messytables.types.DateUtilType
-            ],
-            strict=True
-        )
-        #logger.info('Guessed types: {0}'.format(guessed_types))
-        row_set.register_processor(types_processor(guessed_types, strict=True))
-        row_set.register_processor(stringify_processor())
-
-        ckan_url = data['ckan_url'].rstrip('/')
-
-        datastore_create_request_url = '%s/api/action/datastore_create' % (ckan_url)
-
-        guessed_type_names = [TYPE_MAPPING[type(gt)] for gt in guessed_types]
+        headers = [dict(id=field['id'], type=TYPE_MAPPING.get(field['type'])) for field in fields]
+        print 'headers', headers
+        print 'result', result
 
         def send_request(records):
             request = {'resource_id': data['resource_id'],
-                       'fields': [dict(id=name, type=typename) for name, typename in zip(headers, guessed_type_names)],
+                       'fields': headers,
                        'records': records}
             response = requests.post(datastore_create_request_url,
                              data=json.dumps(request),
@@ -146,40 +116,11 @@ def import_into_datastore(task_id, input):
 
         #logger.info('Creating: {0}.'.format(resource['id']))
 
-        # generates chunks of data that can be loaded into ckan
-        # n is the maximum size of a chunk
-        def chunky(iterable, n):
-            it = iter(iterable)
-            while True:
-                chunk = list(
-                    itertools.imap(
-                        dict, itertools.islice(it, n)))
-                if not chunk:
-                    return
-                yield chunk
-
         count = 0
-        for records in chunky(row_set.dicts(), 100):
+        for records in chunky(result, 100):
             count += len(records)
             send_request(records)
 
+        #from nose.tools import set_trace; set_trace()
+
         #logger.info("There should be {n} entries in {res_id}.".format(n=count, res_id=resource['id']))
-
-        ckan_request_url = ckan_url + '/api/action/resource_update'
-
-        ckan_resource_data = {
-            'id': data["resource_id"],
-            'webstore_url': 'active',
-            'webstore_last_updated': datetime.datetime.now().isoformat(),
-            'url': data['url']
-        }
-
-        response = requests.post(
-            ckan_request_url,
-            data=json.dumps(ckan_resource_data),
-            headers={'Content-Type': 'application/json',
-                     'Authorization': input['apikey']})
-
-        if response.status_code not in (201, 200):
-            raise util.JobError('Ckan bad response code (%s). Response was %s' %
-                                 (response.status_code, response.content))
