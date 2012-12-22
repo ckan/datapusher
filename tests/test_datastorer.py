a609784d0addd2f6c6643f65ff86824c7b492977
Dominik Moritz <domoritz@gmail.com>
2012-12-22 13:55:57 +0100

    Use assert_equal and add tsv test

--- tests/test_datastorer.py
+++ tests/test_datastorer.py
@@ -4,6 +4,7 @@ import uuid
 import time
 import ConfigParser
 
+from nose.tools import assert_equal
 import requests
 from httpretty import HTTPretty
 from httpretty import httprettified
@@ -75,7 +76,7 @@ class TestDatastorer():
         return res_id
 
     @httprettified
-    def test_csv_directly(self):
+    def test_simple_csv_directly(self):
         url = 'http://www.ckan.org/static/simple.csv'
         HTTPretty.register_uri(HTTPretty.GET, url,
                            body=get_static_file('simple.csv'),
@@ -105,12 +106,97 @@ class TestDatastorer():
         assert not 'error' in result, result['error']
 
         value = result['result']['records'][0][u'temperature']
-        assert int(value) == 1, value
-        assert result['result']['total'] == 6, (result['result']['total'], resource_id)
-        assert result['result']['fields'] == [{u'type': u'int4', u'id': u'_id'},
+        assert_equal(int(value), 1)
+        assert_equal(result['result']['total'], 6)
+        assert_equal(result['result']['fields'], [{u'type': u'int4', u'id': u'_id'},
                                               {u'type': u'timestamp', u'id': u'date'},
                                               {u'type': u'numeric', u'id': u'temperature'},
-                                              {u'type': u'text', u'id': u'place'}], result['result']['fields']
+                                              {u'type': u'text', u'id': u'place'}])
+
+    @httprettified
+    def test_simple_tsv_directly(self):
+        url = 'http://www.ckan.org/static/simple.tsv'
+        HTTPretty.register_uri(HTTPretty.GET, url,
+                           body=get_static_file('simple.tsv'),
+                           content_type="application/tsv")
+        resource_id = self.make_resource_id()
+
+        data = {
+            'metadata': {'key': 'value'},
+            'apikey': self.api_key,
+            'job_type': 'import_into_datastore',
+            'data': {
+                'url': url,
+                'format': 'tsv',
+                'ckan_url': 'http://%s/' % self.host,
+                'resource_id': resource_id
+            }
+        }
+
+        ds.import_into_datastore(None, data)
+
+        response = requests.get(
+            'http://%s/api/action/datastore_search?resource_id=%s' % (self.host, resource_id),
+             headers={"content-type": "application/json"})
+
+        result = json.loads(response.content)
+
+        assert not 'error' in result, result['error']
+
+        value = result['result']['records'][0][u'temperature']
+        assert_equal(int(value), 1)
+        assert_equal(result['result']['total'], 6)
+        assert_equal(result['result']['fields'], [{u'type': u'int4', u'id': u'_id'},
+                                              {u'type': u'timestamp', u'id': u'date'},
+                                              {u'type': u'numeric', u'id': u'temperature'},
+                                              {u'type': u'text', u'id': u'place'}])
+
+    @httprettified
+    def test_october_2011_directly(self):
+        url = 'http://www.ckan.org/static/october_2011.csv'
+        HTTPretty.register_uri(HTTPretty.GET, url,
+                           body=get_static_file('october_2011.csv'),
+                           content_type="application/csv")
+        resource_id = self.make_resource_id()
+
+        data = {
+            'metadata': {'key': 'value'},
+            'apikey': self.api_key,
+            'job_type': 'import_into_datastore',
+            'data': {
+                'url': url,
+                'format': 'csv',
+                'ckan_url': 'http://%s/' % self.host,
+                'resource_id': resource_id
+            }
+        }
+
+        ds.import_into_datastore(None, data)
+
+        response = requests.get(
+            'http://%s/api/action/datastore_search?resource_id=%s' % (self.host, resource_id),
+             headers={"content-type": "application/json"})
+
+        result = json.loads(response.content)
+
+        assert not 'error' in result, result['error']
+
+        value = result['result']['records'][0][u'Supplier Name']
+        assert_equal(value, 'ALBANY OFFICE FURNITURE SOLUTIONS')
+        assert_equal(result['result']['total'], 230)
+        assert_equal(len(result['result']['records']), 100)
+
+        assert_equal(result['result']['fields'], [{u'type': u'int4', u'id': u'_id'},
+                                              {u'type': u'text', u'id': u'Directorate'},
+                                              {u'type': u'text', u'id': u'Service Area'},
+                                              {u'type': u'text', u'id': u'Expenditure Category'},
+                                              {u'type': u'timestamp', u'id': u'Payment Date'},
+                                              {u'type': u'text', u'id': u'Supplier Name'},
+                                              {u'type': u'numeric', u'id': u'Internal Ref'},
+                                              {u'type': u'text', u'id': u'Capital/ Revenue'},
+                                              {u'type': u'text', u'id': u'Cost Centre'},
+                                              {u'type': u'text', u'id': u'Cost Centre Description'},
+                                              {u'type': u'float8', u'id': u'Grand Total'}])
 
     @httprettified
     def test_csv_file(self):
@@ -152,9 +238,9 @@ class TestDatastorer():
         assert not 'error' in result, result['error']
 
         value = result['result']['records'][0][u'temperature']
-        assert int(value) == 1, value
-        assert result['result']['total'] == 6, (result['result']['total'], resource_id)
-        assert result['result']['fields'] == [{u'type': u'int4', u'id': u'_id'},
+        assert_equal(int(value), 1)
+        assert_equal(result['result']['total'], 6)
+        assert_equal(result['result']['fields'], [{u'type': u'int4', u'id': u'_id'},
                                               {u'type': u'timestamp', u'id': u'date'},
                                               {u'type': u'numeric', u'id': u'temperature'},
-                                              {u'type': u'text', u'id': u'place'}], result['result']['fields']
+                                              {u'type': u'text', u'id': u'place'}])
