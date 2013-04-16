0f50ec3684e2f72a717a5c98237304b68bf290ad
Nigel Babu <nigelbabu@gmail.com>
2013-04-16 10:59:03 +0530

    Remove gunicorn.py and create wsgi.py one level up

--- wsgi.py
+++ wsgi.py
@@ -1,9 +1,7 @@
 import ckanserviceprovider.web as web
-import jobs
+import datapusher.jobs as jobs
 
 # check whether jobs have been imported properly
 assert(jobs.push_to_datastore)
-
-
 web.configure()
 app = web.app
