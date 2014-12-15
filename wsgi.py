19b43280436fa63d08fc95a98a74dc8a612b77c1
Sean Hammond <git@seanh.cc>
2014-12-15 14:24:53 +0000

    Fix wsgi.py
    Was broken by recent refactoring.

--- wsgi.py
+++ wsgi.py
@@ -4,5 +4,5 @@ import datapusher.jobs as jobs
 # check whether jobs have been imported properly
 assert(jobs.push_to_datastore)
 
-web.configure()
-app = web.app
+web.init()
+web.app.run(web.app.config.get('HOST'), web.app.config.get('PORT'))
