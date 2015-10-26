75d536e7b0f697ce2f8dd2d37d2917c970100c20
amercader <amercadero@gmail.com>
2015-10-26 12:14:45 +0000

    Load settings before jobs in dev wsgi file (same as 3147035f)

--- wsgi.py
+++ wsgi.py
@@ -2,10 +2,11 @@
 # install) use deployment/datapusher.wsgi
 
 import ckanserviceprovider.web as web
-import datapusher.jobs as jobs
 
+web.init()
+
+import datapusher.jobs as jobs
 # check whether jobs have been imported properly
 assert(jobs.push_to_datastore)
 
-web.init()
 web.app.run(web.app.config.get('HOST'), web.app.config.get('PORT'))
