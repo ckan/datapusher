874a2adbfc98cb8c3b01ad075d76204428fa1c6f
Marko Bocevski <marko.bocevski@keitaro.com>
2020-09-10 17:15:58 +0200

    Make chunks and timeout configurable at runtime

--- datapusher/jobs.py
+++ datapusher/jobs.py
@@ -30,8 +30,8 @@ else:
     locale.setlocale(locale.LC_ALL, '')
 
 MAX_CONTENT_LENGTH = web.app.config.get('MAX_CONTENT_LENGTH') or 10485760
-CHUNK_SIZE = 16 * 1024  # 16kb
-DOWNLOAD_TIMEOUT = 30
+CHUNK_SIZE = web.app.config.get('CHUNK_SIZE') or 16384
+DOWNLOAD_TIMEOUT = web.app.config.get('DOWNLOAD_TIMEOUT') or 30
 
 if web.app.config.get('SSL_VERIFY') in ['False', 'FALSE', '0', False, 0]:
     SSL_VERIFY = False
