3ad15df0840673cdc663c5110e4ab6006caf2dae
Marko Bocevski <marko.bocevski@keitaro.com>
2020-10-08 12:56:00 +0200

    Remove python 2.7 support

--- setup.py
+++ setup.py
@@ -42,7 +42,6 @@ setup(
 
         # Specify the Python versions you support here. In particular, ensure
         # that you indicate whether you support Python 2, Python 3 or both.
-        'Programming Language :: Python :: 2.7',
         'Programming Language :: Python :: 3.6',
         'Programming Language :: Python :: 3.7',
         'Programming Language :: Python :: 3.8',
@@ -63,8 +62,7 @@ setup(
     install_requires=[],
 
     # If there are data files included in your packages that need to be
-    # installed, specify them here.  If using Python 2.6 or less, then these
-    # have to be included in MANIFEST.in as well.
+    # installed, specify them here.
     package_data={
     },
 
