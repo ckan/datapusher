18a1e7e6c66b1fe6b253482d3a13f66d10777110
GauravPandey-NECI <66116382+Gauravp-NEC@users.noreply.github.com>
2022-08-01 15:51:17 +0530

    Update jobs.py

--- datapusher/jobs.py
+++ datapusher/jobs.py
@@ -508,8 +508,9 @@ def push_to_datastore(task_id, input, dry_run=False):
     for i, chunk in enumerate(chunky(result, CHUNK_INSERT_ROWS)):
         records, is_it_the_last_chunk = chunk
         count += len(records)
-        logger.info('Saving records {number} - {record}'.format(
-            number=1, record=count if is_it_the_last_chunk else ''))
+        if is_it_the_last_chunk:
+            logger.info('Saving records {number} - {record}'.format(
+                number=1, record=count))
         send_resource_to_datastore(resource, headers_dicts, records,
                                    is_it_the_last_chunk, api_key, ckan_url)
 
